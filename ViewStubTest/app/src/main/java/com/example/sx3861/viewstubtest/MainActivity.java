ec6cd2fd20dabefb618f393a820818776a7c0723
lazysunshinesmile <1033427175@qq.com>
2018-08-14 10:45:39 +0800

    第三天暂时告一段落

--- ViewStubTest/app/src/main/java/com/example/sx3861/viewstubtest/MainActivity.java
+++ ViewStubTest/app/src/main/java/com/example/sx3861/viewstubtest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sx3861.viewstubtest;
 
 import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Debug;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,13 +11,19 @@ import android.view.View;
 import android.view.ViewStub;
 import android.widget.Button;
 
+import com.squareup.haha.perflib.Main;
+
 import java.io.File;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
+
+    private WeakReference<Context> context = new WeakReference<Context>(this);
+    private static WeakReference<String> USER_NAME = new WeakReference<String>("sunxiang");
 
     private ViewStub viewStub;
     private Button button;
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 getCurrentProcessMemInfo();
                 getAllProcessMemInfo();
                 newThreadNullPoint();
+                weakReferenceUse();
             }
         });
 //        Debug.stopMethodTracing();
@@ -109,5 +117,32 @@ public class MainActivity extends AppCompatActivity {
                 al.get(0);
             }
         }).start();
+    }
+
+    private void weakReferenceUse() {
+        USER_NAME.get();
+        USER_NAME.enqueue();
+    }
+
+    public static class MemLeakTest {
+
+        private String name = "sunxiang";
+
+        private Button btn;
+
+        private void init() {
+            //这个是静态内部类，不能直接调用外部类的方法，说明静态内部类没有持有外部类的引用。所以不会造成内存泄漏。
+//            newThreadNullPoint();
+            //new View.OnClickListener() 相对于MemLeakTest来说，他就是一个非静态匿名内部类。
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //下面两句话等同。能够直接调用外部类的变量name，说明他持有了我外部类的引用，
+                    MemLeakTest.this.name = "sunxiang2";
+                    name = "sunxiang2";
+
+                }
+            });
+        }
     }
 }
