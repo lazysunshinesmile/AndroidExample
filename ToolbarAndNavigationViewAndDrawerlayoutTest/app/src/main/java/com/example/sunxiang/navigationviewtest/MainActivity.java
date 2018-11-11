9ea73cb1e2169ca70cd95d6673c1c767a54c5cdc
sun xiang <xiang.sun@qianli-inc.com>
2018-11-11 21:14:35 +0800

    Navigationview + toolbar + Drawerlayout的学习

--- ToolbarAndNavigationViewAndDrawerlayoutTest/app/src/main/java/com/example/sunxiang/navigationviewtest/MainActivity.java
+++ ToolbarAndNavigationViewAndDrawerlayoutTest/app/src/main/java/com/example/sunxiang/navigationviewtest/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.sunxiang.navigationviewtest;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,17 +21,62 @@ import java.lang.reflect.Method;
 public class MainActivity extends AppCompatActivity {
 
   private Toolbar mToolBar;
+  private NavigationView navigationView;
+  private DrawerLayout drawerLayout;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //显示toolbar
+    //navigation 在toolbar下方
+//    setContentView(R.layout.activity_main2);
+
     mToolBar = findViewById(R.id.toobar);
+    navigationView = findViewById(R.id.navigation_view);
+    drawerLayout = findViewById(R.id.drawerlayou);
+
+    //显示toolbar
     setSupportActionBar(mToolBar);
     //设置Navigation 默认是返回键
     ActionBar actionBar = getSupportActionBar();
+
+    //设置返回键显示
+    actionBar.setHomeButtonEnabled(true);
     actionBar.setDisplayHomeAsUpEnabled(true);
 
+    //设置左上角返回键图标，默认是返回
+    actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+
+    //以下设置了左上角的功能图标，打开及关闭navigation。有如下语句，就可以注释onOptionsItemSelected中的case android.R.id.home:。
+    ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, mToolBar, R.string.app_name,
+        R.string.app_name);
+    //该方法会自动和actionBar关联, 将开关的图片显示在了action上，如果不设置，也可以有抽屉的效果，不过是默认的图标
+    drawerToggle.syncState();
+    drawerLayout.addDrawerListener(drawerToggle);
+
+    initListner();
+
+
+  }
+
+  private void initListner() {
+    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+          case R.id.first_id:
+            showToast("navigationview first id");
+            break;
+          case R.id.second_id:
+            showToast("navigationview second id");
+            break;
+          case R.id.third_id:
+            showToast("navigationview third id");
+            break;
+        }
+        return false;
+      }
+    });
   }
 
   //为了让overflow中的选项显示图标，必须使用反射
@@ -54,6 +104,11 @@ public class MainActivity extends AppCompatActivity {
     switch (item.getItemId()) {
       case android.R.id.home:
         showToast("点击了返回键");
+//        if (!drawerLayout.isDrawerOpen(navigationView)){
+//          drawerLayout.openDrawer(navigationView);
+//        } else {
+//          drawerLayout.closeDrawer(Gravity.LEFT);
+//        }
         break;
       case R.id.first_id:
         showToast("点击第一个menu");
